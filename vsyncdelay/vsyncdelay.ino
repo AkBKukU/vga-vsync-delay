2fa1efd32041bf44786cf2fe642da12f9dc79e65
Shelby Jueden <shelby@jueden.org>
2020-10-17 15:32:22 -0700

    auto detect resolution change and only enable for 480p

--- vsyncdelay/vsyncdelay.ino
+++ vsyncdelay/vsyncdelay.ino
@@ -8,9 +8,9 @@
 #include <stdlib.h>
  
 #define VSYNC_OUT 13 // PORT B pin 5
-#define VSYNC_EN_ORIGINAL 6 // PORT B pin 5
-#define VSYNC_EN_DELAY 7 // PORT B pin 5
-#define LED_EXTRA 8 // PORT B pin 
+#define VSYNC_EN_ORIGINAL 6
+#define VSYNC_EN_DELAY 7
+#define LED_EXTRA 8
 #define VSYNC_IN   2
 #define HSYNC_IN   3
 
@@ -25,15 +25,13 @@ volatile int h_offset = 8;
 void vsync()
 {
 
-	/*
-	if (h_count == VGA_LINES) {
-		h_offset = h_offset_store;
+	if (h_count == VGA_LINES && h_offset_store != 0) {
+		syncDelayActive( true );
 	} else {
-		h_offset = 0;
+		syncDelayActive( false );
 	}
-	*/
+
 	h_count = 0;
-	v_count+=1;
 }
 
 void hsync()
@@ -47,6 +45,18 @@ void hsync()
 
 	h_count+=1;
 
+}
+
+
+void syncDelayActive(bool setting)
+{
+	digitalWrite(VSYNC_EN_DELAY,setting);
+	digitalWrite(VSYNC_EN_ORIGINAL,!setting);
+	if ( setting ) {
+		h_offset = h_offset_store;
+	} else {
+		h_offset = 0;
+	}
 }
 
 // the setup routine runs once when you press reset:
@@ -72,16 +82,9 @@ void loop()
 		if ( Serial.read() == ':' ) {
 			int new_delay = Serial.parseInt();
 			Serial.println(":"+String(new_delay));
-			h_offset = new_delay;
-			h_offset_store = h_offset;
-			Serial.println("h: "+String(h_offset));
-			if ( h_offset == 0 ) {
-				digitalWrite(VSYNC_EN_ORIGINAL,HIGH);
-				digitalWrite(VSYNC_EN_DELAY,LOW);
-			} else {
-				digitalWrite(VSYNC_EN_ORIGINAL,LOW);
-				digitalWrite(VSYNC_EN_DELAY,HIGH);
-			}
+			h_offset_store = new_delay;
+			Serial.println("h: "+String(h_offset_store));
+			syncDelayActive( h_offset_store != 0 );
 		}
 	}
 
